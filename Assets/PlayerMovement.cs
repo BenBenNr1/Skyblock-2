eb0908139b367e633a066c7952587c83dd68f239
Benj4fade <benja.myplay@gmail.com>
2025-06-08 10:57:32 +0200

    Movement (Budget)

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float xRotSpeed;
+    [SerializeField] float yRotSpeed;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0) { rb.AddForce(transform.right * Input.GetAxis("Horizontal")); }
+        if (Input.GetAxis("Vertical") != 0) { rb.AddForce(transform.forward * Input.GetAxis("Vertical")); }
+
+        RotateCam();
+    }
+
+    private void RotateCam()
+    {
+        float xRot = Input.mousePositionDelta.x * Time.deltaTime;
+        float yRot = Input.mousePositionDelta.y * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + xRot * xRotSpeed, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(
+            Camera.main.transform.eulerAngles.x + -yRot * yRotSpeed, 
+            Camera.main.transform.eulerAngles.y, 
+            Camera.main.transform.eulerAngles.z);
+    }
+}
